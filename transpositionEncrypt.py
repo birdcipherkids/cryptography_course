db9cc26a7dea549711fdff6838c89c3890564b63
Geovanny <birdcode.py@gmail.com>
2025-02-01 12:12:36 -0500

    Transposition encrypt algorithm

---
+++ transpositionEncrypt.py
@@ -0,0 +1,36 @@
+# Transposition encrypt
+
+import pyperclip
+
+def main():
+
+	myMessage = 'Common sense is not so common'
+	myKey = 8
+
+	cipherText = encryptMessage(myKey, myMessage)
+
+	print(cipherText + '|')
+	pyperclip.copy(cipherText)
+
+
+def encryptMessage(key, message):
+
+	cipherText = [''] * key
+
+	for col in range(key):
+
+		pointer = col
+
+		while pointer < len(message):
+
+			cipherText[col] = cipherText[col] + message[pointer]
+			pointer = pointer + key
+
+
+	return ''.join(cipherText)
+
+
+
+if __name__ == '__main__':
+
+	main()
